ede2325468a00d3deb595123d1fcd9c3c82e0b57
Hazy <Wchodz3WTo@gmail.com>
2024-08-05 11:48:26 +0200

    Adjusted local storage version number to reflect game version

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ import updateLoadout from "./data/updateLoadout";
 
 const page = ref(localStorage.getItem("page") || "tracker");
 
-const version = "1.3.6";
+const version = "24.2.0";
 
 const changePage = (p) => {
   localStorage.setItem("page", p);
