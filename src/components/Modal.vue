5e2ff4433bb388d12dbef78aa9c9c9fdd5f59295
Hazy <Wchodz3WTo@gmail.com>
2025-02-25 14:48:42 +0100

    Fix modal bg

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport to="#app">
     <div
-      class="min-h-screen w-full bg-[#131313]/90 flex items-center justify-center fixed z-50 left-0 top-0 p-3"
+      class="min-h-screen w-full bg-stone-950/90 flex items-center justify-center fixed z-50 left-0 top-0 p-3"
       id="modal"
     >
       <div class="bg-theme p-4 flex flex-col gap-2 border border-solid border-slate-300/15 rounded-md max-h-[85vh] max-w-[1360px] w-full overflow-auto">
