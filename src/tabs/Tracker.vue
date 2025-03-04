16f90d611a0f345fff78204df5838f3d1cfa3448
Hazy <Wchodz3WTo@gmail.com>
2025-03-04 14:46:24 +0100

    Minor changes

--- src/tabs/Tracker.vue
+++ src/tabs/Tracker.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full max-w-4xl flex flex-col gap-4">
     <CardSlots />
-    <div class="flex gap-2 items-center bg-slate-600/5 border border-solid border-slate-300/30 rounded-md p-3.5">
+    <div class="flex gap-2 items-center border border-solid border-slate-300/30 rounded-md p-3.5">
       <Icon
         icon="octicon:info-16"
-        height="32"
+        height="28"
       />
-      <p class="text-sm">Use lab calculator you can find <a href="https://thetower.tools/">thetower.tools</a> to discover cards and masteries labs times and costs</p>
+      <p class="text-sm">Use lab calculator you can find <a href="https://thetower.tools/" class="text-sky-600">thetower.tools</a> to discover cards and masteries labs times and costs</p>
     </div>
     <div class="self-center bg-nav p-2 rounded-lg flex gap-4">
       <button
